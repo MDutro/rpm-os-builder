cd389f5a362d756b26bda4e782cfbdcae99fa790
MDutro <mdutrolaw@gmail.com>
2025-04-29 14:04:16 -0700

    Add build files from Bluefin LTS

--- Containerfile
+++ Containerfile
@@ -1,9 +1,12 @@
 # Allow build scripts to be referenced without being copied into the final image
 FROM scratch AS ctx
 COPY build_files /
+COPY system_files /files
+COPY system_files_overrides /overrides
+COPY build_scripts /build_scripts
 
 # Base Image
-FROM ghcr.io/ublue-os/bluefin-dx:stable
+FROM ghcr.io/ublue-os/cosmic-atomic-main:42
 
 ## Other possible base images include:
 # FROM ghcr.io/ublue-os/bazzite:latest
@@ -18,13 +21,24 @@ FROM ghcr.io/ublue-os/bluefin-dx:stable
 ## make modifications desired in your image and install packages by modifying the build.sh script
 ## the following RUN directive does all the things required to run "build.sh" as recommended.
 
-RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
-    --mount=type=cache,dst=/var/cache \
-    --mount=type=cache,dst=/var/log \
-    --mount=type=tmpfs,dst=/tmp \
-    /ctx/build.sh && \
-    ostree container commit
+# RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+#     --mount=type=cache,dst=/var/cache \
+#     --mount=type=cache,dst=/var/log \
+#     --mount=type=tmpfs,dst=/tmp \
+#     /ctx/build.sh && \
+#     ostree container commit
+
+RUN --mount=type=tmpfs,dst=/opt \
+  --mount=type=tmpfs,dst=/tmp \
+  --mount=type=tmpfs,dst=/var \
+  --mount=type=tmpfs,dst=/boot \
+  --mount=type=bind,from=context,source=/,target=/run/context \
+  /run/context/build_scripts/build.sh
+
+# Makes `/opt` writeable by default
+# Needs to be here to make the main image build strict (no /opt there)
+RUN rm -rf /opt && ln -s /var/opt /opt 
     
 ### LINTING
 ## Verify final image and contents are correct.
-RUN bootc container lint
+# RUN bootc container lint
---
+++ build_scripts/10-packages-image-base.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+# This is the base for a minimal GNOME system on CentOS Stream.
+
+# This thing slows down downloads A LOT for no reason
+dnf remove -y subscription-manager
+
+dnf -y install centos-release-hyperscale-kernel
+dnf config-manager --set-disabled "centos-hyperscale,centos-hyperscale-kernel"
+dnf --enablerepo="centos-hyperscale" --enablerepo="centos-hyperscale-kernel" -y update kernel
+
+# The base images take super long to update, this just updates manually for now
+dnf -y install 'dnf-command(versionlock)'
+dnf versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-uki-virt
+
+dnf -y install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${MAJOR_VERSION_NUMBER}.noarch.rpm"
+dnf config-manager --set-enabled crb
+
+# Multimidia codecs
+dnf -y install @multimedia gstreamer1-plugins-{bad-free,bad-free-libs,good,base} lame{,-libs} libjxl
+
+# `dnf group info Workstation` without GNOME
+dnf group install -y --nobest \
+	-x rsyslog* \
+	-x cockpit \
+	-x cronie* \
+	-x crontabs \
+	-x PackageKit \
+	-x PackageKit-command-not-found \
+	"Common NetworkManager submodules" \
+	"Core" \
+	"Fonts" \
+	"Guest Desktop Agents" \
+	"Hardware Support" \
+	"Printing Client" \
+	"Standard" \
+	"Workstation product core"
+
+# Minimal GNOME group. ("Multimedia" adds most of the packages from the GNOME group. This should clear those up too.)
+# In order to reproduce this, get the packages with `dnf group info GNOME`, install them manually with dnf install and see all the packages that are already installed.
+# Other than that, I've removed a few packages we didnt want, those being a few GUI applications.
+dnf -y install \
+	-x PackageKit \
+	-x PackageKit-command-not-found \
+	"NetworkManager-adsl" \
+	"libsane-hpaio" \
+	"ptyxis" \
+	"sane-backends-drivers-scanners" \
+	"yelp-tools"
+
+dnf -y install \
+	plymouth \
+	plymouth-system-theme \
+	fwupd \
+	systemd-{resolved,container,oomd} \
+	libcamera{,-{v4l2,gstreamer,tools}} \
+	ffmpegthumbnailer
+
+# This package adds "[systemd] Failed Units: *" to the bashrc startup
+dnf -y remove console-login-helper-messages
